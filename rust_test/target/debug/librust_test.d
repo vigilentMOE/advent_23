d92e6ff88cfd25ad024026af660fcd543699c28e
vigilentMOE <rockred12@outlook.com>
2023-12-02 17:51:27 -0500

    ci: trying to fix rust ci

---
+++ rust_test/target/debug/librust_test.d
@@ -0,0 +1,1 @@
+/home/vigmoe/advent/advent_23/rust_test/target/debug/librust_test.rlib: /home/vigmoe/advent/advent_23/rust_test/src/functionality.rs /home/vigmoe/advent/advent_23/rust_test/src/lib.rs
